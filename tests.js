6e18ee94c4bca0c967d9ab2be9a7b691062e32b2
Carlos <carlosdiones28@gmail.com>
2022-05-09 15:08:23 -0300

    files used only for tests

--- tests.js
+++ tests.js
@@ -1,10 +1,5 @@
-import {invoke} from './invoke.js';
+import {sortrows, makeSortable} from './sortrows.js';
 
-
-let num = 0;
-
-function add1(){
-    console.log(num+=10);
-}
-
-invoke(add1, 0, 100, 5000);
+const el = document.getElementById('myTable');
+makeSortable(el);
+// sortrows(el, 1);
